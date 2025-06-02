306bfa3accd5e74ed951baae7f8cf1bd65ee7b69
Skuffy <skuffy808@gmail.com>
2025-06-02 21:33:40 +1200

    add credits

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -6,3 +6,7 @@ func _on_start_pressed() -> void:
 
 func _on_options_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/options_menu.tscn")
+
+
+func _on_credits_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/credits.tscn")
