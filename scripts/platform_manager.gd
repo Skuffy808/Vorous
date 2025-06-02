d9947d0e22507a5f99bce16e24fc567efeeed123
Skuffy <skuffy808@gmail.com>
2025-06-02 18:19:01 +1200

    add random platform thing

--- scripts/platform_manager.gd
+++ scripts/platform_manager.gd
@@ -2,14 +2,26 @@ extends Node
 
 @onready var platform_1: Node2D = $Platform1
 @onready var player: CharacterBody2D = $"../Player"
+
 const PLATFORM_1 = preload("res://scenes/Platforms/platform_1.tscn")
+const PLATFORM_2 = preload("res://scenes/Platforms/platform_2.tscn")
 
 var spawn_new_pos := 640
 
-func _process(delta):
-	if player.position.x > spawn_new_pos:
-		print("Hello, World!")
+func spawn_random_platform():
+	var platformRNG = randi_range(1, 10)
+	if platformRNG > 5:
 		var new_platform = PLATFORM_1.instantiate()
 		new_platform.position.x = spawn_new_pos * 2
 		add_child(new_platform)
+	elif platformRNG < 5:
+		var new_platform = PLATFORM_2.instantiate()
+		new_platform.position.x = spawn_new_pos * 2
+		add_child(new_platform)
+	else:
+		print("WHAT IN THE ACTUAL FUCK HAPPENED")
+		
+func _process(delta):
+	if player.position.x > spawn_new_pos:
+		spawn_random_platform()
 		spawn_new_pos += 640
