27d6a328c03754b8de67a69b1c8cc33315040273
Skuffy <skuffy808@gmail.com>
2025-06-03 18:18:11 +1200

    add zoom

---
+++ scripts/camera_zoom.gd
@@ -0,0 +1,14 @@
+extends Camera2D
+
+const MAX_ZOOM = 2
+const MIN_ZOOM = 1
+const ZOOM_STEP = 0.1
+
+func _process(_delta: float) -> void:
+	if Input.is_action_just_pressed("zoom_in"):
+		zoom += Vector2(ZOOM_STEP, ZOOM_STEP)
+	elif Input.is_action_just_pressed("zoom_out"):
+		zoom -= Vector2(ZOOM_STEP, ZOOM_STEP)
+
+	zoom.x = clamp(zoom.x, MIN_ZOOM, MAX_ZOOM)
+	zoom.y = clamp(zoom.y, MIN_ZOOM, MAX_ZOOM)
