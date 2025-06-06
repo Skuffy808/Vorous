edd9d95358a9e1e46779be33edcefb8e1d6da1bd
Skuffy <skuffy808@gmail.com>
2025-06-07 10:38:47 +1200

    improve main menu

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,12 +1,12 @@
 extends Control
 
-func _on_start_pressed() -> void:
+func _on_play_texture_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/game.tscn")
 
 
-func _on_options_pressed() -> void:
+func _on_options_texture_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/options_menu.tscn")
 
 
-func _on_credits_pressed() -> void:
+func _on_credits_texture_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/credits.tscn")
