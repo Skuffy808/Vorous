2a84ed26bb5275e5e797fc87b1350fad73d059eb
Skuffy <skuffy808@gmail.com>
2025-06-07 17:45:05 +1200

    add walking sound

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,21 +1,24 @@
 extends CharacterBody2D
 
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+@onready var audio_stream_player: AudioStreamPlayer = $AudioStreamPlayer
 
 var speed = 200.0
 const JUMP_VELOCITY = -300.0
 var jumped = false
 
 func _physics_process(delta: float) -> void:
-	# Add gravity
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 		if jumped:
 			animated_sprite_2d.play("jump")
+		audio_stream_player.stop()
 	else:
 		if jumped:
 			jumped = false
 		animated_sprite_2d.play("run")
+		if not audio_stream_player.playing:
+			audio_stream_player.play()
 
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
