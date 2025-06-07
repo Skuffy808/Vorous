f91e5294ddecf8e815b729b78a84e7bcce01caaa
Skuffy <skuffy808@gmail.com>
2025-06-07 17:22:42 +1200

    update death menu

--- scripts/death_menu.gd
+++ scripts/death_menu.gd
@@ -1,10 +1,27 @@
 extends Node2D
 
+@onready var player: CharacterBody2D = $"../../Player"
+@onready var stopwatch: Node2D = $"../Stopwatch"
+@onready var timer: Timer = $Timer
+@onready var death_menu: Node2D = $"."
+@onready var audio_stream_player: AudioStreamPlayer = $AudioStreamPlayer
+
 func _on_respawn_texture_button_pressed() -> void:
 	Engine.time_scale = 1
 	get_tree().reload_current_scene()
 
-
 func _on_menu_texture_button_pressed() -> void:
 	Engine.time_scale = 1
 	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
+
+func player_died():
+	audio_stream_player.play()
+	print("Hello, World!")
+	stopwatch.visible = false
+	Engine.time_scale = 0.1
+	timer.start()
+	player.collision_layer = 0
+	player.collision_mask = 0
+
+func _on_timer_timeout() -> void:
+	death_menu.visible = true
--- scripts/killzone.gd
+++ scripts/killzone.gd
@@ -1,19 +1,10 @@
 extends Area2D
 
-@onready var timer: Timer = $Timer
-@onready var death_menu: Node2D = $"../CanvasLayer/DeathMenu"
-@onready var stopwatch: Node2D = $"../CanvasLayer/Stopwatch"
-@onready var player: CharacterBody2D = $"../Player"
+
+@onready var death_menu = get_tree().get_root().get_node("Game/CanvasLayer/DeathMenu")
+
+
 
 func _on_body_entered(body: Node2D) -> void:
-	stopwatch.visible = false
-	Engine.time_scale = 0.1
-	timer.start()
-	player.collision_layer = 0
-	player.collision_mask = 0
-
-	
-func _on_timer_timeout() -> void:
-	death_menu.visible = true
-
+	death_menu.player_died()
 	
